f750576a0b5c7040919aaa07d5687af79ae6b032
Tahis9 <147927771+tahis9@users.noreply.github.com.>
2025-12-28 11:54:43 +1100

    Update omnibar to catexpo expo

--- graphics/omnibar/omnibar.js
+++ graphics/omnibar/omnibar.js
@@ -1,213 +1,77 @@
-const charityLogo = nodecg.Replicant('assets:omnibarCharityLogo');
-const marathonLogo = nodecg.Replicant('assets:omnibarMarathonLogo');
+const speedcontrolBundle = 'nodecg-speedcontrol';
 const runDataArray = nodecg.Replicant('runDataArray', 'nodecg-speedcontrol');
 const runDataActiveRun = nodecg.Replicant('runDataActiveRun', 'nodecg-speedcontrol');
+const nextRuns = nodecg.Replicant('nextRuns');
 
-let text = {};
-let counter = 0;
-let dataVal = {};
+let host = '';
+let staticIndex, runIndex, targetIndex, pollIndex, rewardIndex;
+rewardIndex = pollIndex = targetIndex = runIndex = 10;
+staticIndex = 0;
 
-NodeCG.waitForReplicants(data, charityLogo, marathonLogo, runDataArray, runDataActiveRun).then(() => {
-	try { document.querySelector('#charityLogo img').setAttribute('src', charityLogo.value[0].url) } catch {}
-	try { document.querySelector('#marathonLogo img').setAttribute('src', marathonLogo.value[0].url) } catch {}
-	runDataActiveRun.on('change', (newVal) => data.value.host = (newVal.customData && newVal.customData.host) ? newVal.customData.host : undefined)
-	data.on('change', (newVal, oldVal) => { 
-		if (!oldVal) setText('message', 0, data.value)
-		dataVal = newVal;
-	})
-	changeOmnibarText()
-});
+NodeCG.waitForReplicants(runDataArray, runDataActiveRun).then(() => {
 
-async function setText(type, index, value) {
-	let players;
-	if (type === "run") players = await getPlayerNames(value.runs[index]);
+	// Please view the Github docs for valid values.
+	// Set static text.	
+	function staticText(index) {
+		if (index === 0)
+			setOmnibarHtml(`<p class="is-single-line is-text-centered">Follow us on Twitch: pokemonspeedrunstv</p>`)
+		else if (index === 1)
+			setOmnibarHtml(`<p class="is-single-line is-text-centered">Follow us on YouTube: @PokemonSpeedRunsPSR</p>`)
+		else if (index === 2)
+			setOmnibarHtml(`<p class="is-single-line is-text-centered">Follow us on Twitter/X: @PkmnSpeedRuns</p>`)
+		else if (index === 3)
+			setOmnibarHtml(`<p class="is-single-line is-text-centered">Follow us on Bluesky: @pokemonspeedruns.bsky.social</p>`)
+		else if (index === 4)
+			setOmnibarHtml(`<p class="is-single-line is-text-centered">Follow us on Instagram: @pkmnspeedruns</p>`)
+		else 
+			setOmnibarHtml(`<p class="is-single-line is-text-centered">Join the PSR Subreddit: r/pkmnspeedruns</p>`)
+	}
 
-	// Omnibar Text
-	try {
-		if (type === "message") {
-			text.message = [
-				`<p class="singleLine">Indiethon 2022 is benefiting <span class="singleLine secondary">Puppies Behind Bars</span></p>`,
-				`<p class="singleLine">Donate at <span class="singleLine secondary">donate.indiethon.live</span></p>`,
-				`<p class="singleLine">Check out our merchandise at<span class="singleLine secondary"> store.indiethon.live</span></p>`,
-			];
-		}
+	// Run text.
+	function runText(index) {
+		const players = getNamesForRun(nextRuns.value.data[index]).join(', ');
+		if (index === 0)
+			setOmnibarHtml(`<p class='is-multiline is-text-centered'>UP NEXT: ${nextRuns.value.data[index].game}</p> <p class='is-multiline is-text-centered'>${nextRuns.value.data[index].category} by ${players}</p>`)
+		else
+			setOmnibarHtml(`<p class="is-multiline is-text-centered">ON DECK: ${nextRuns.value.data[index].game}</p> <p class="is-multiline is-text-centered">${nextRuns.value.data[index].category} by ${players}</p>`)
+	}
 
-		else if (type === "host") {
-			text.host = [
-				`<p class="singleLine">Your current host is <span class="singleLine secondary">${value.host}</span></p>`
-			];
-		}
-
-		else if (type === "run") {
-			text.run = [
-				`<p class="multiLine">Up Next: ${value.runs[index].game}</p> <p class="multiLine">${value.runs[index].category} by ${players}</p>`,
-				`<p class="multiLine">On Deck: ${value.runs[index].game}</p> <p class="multiLine">${value.runs[index].category} by ${players}</p>`,
-			];
-		}
-
-		else if (type === "target") {
-			text.target = [
-				`<p class="multiLine">Upcoming Incentive: ${value.targets[index].name}</p> <p class="multiLine">$${value.targets[index].total} / $${value.targets[index].goal}</p>`,
-				`<p class="multiLine">Upcoming Incentive: ${value.targets[index].name}</p> <p class="multiLine">$${value.targets[index].total} / $${value.targets[index].goal}</p>`,
-			]
-		}
-
-		else if (type === "bidwar") {
-			text.bidwar = [
-				`<p class="multiLine">Upcoming Bidwar: ${value.bidwars[index].name}</p>`,
-				`<p class="multiLine">Upcoming Bidwar: ${value.bidwars[index].name}</p>`,
-			]
-			text.bidwar[index] += `<p class="multiLine">`;
-			for (let i = 0; i < value.bidwars[index].options.length; i++) {
-				let option = value.bidwars[index].options[i];
-				text.bidwar[index] += `${option.name}: $${option.total}`;
-				if (i === value.bidwars[index].options.length - 1 || i >= 2) break;
-				else text.bidwar[index] += ` vs `;
+	// Do not edit below this line (unless you know what you're doing!)
+	runDataActiveRun.on('change', (newVal, oldVal) => {
+		let runs = [];
+		let currentRunIndex = runDataArray.value.findIndex(runData => runData.id === newVal.id);
+		for (let i = 1; i <= nodecg.bundleConfig.omnibar.numRuns; i++) {
+			if (!runDataArray.value[currentRunIndex + i]) {
+				break;
 			}
-			text.bidwar[index] += `</p>`
+			runs.push(runDataArray.value[currentRunIndex + i]);
 		}
+		nextRuns.value = { dataLength: runs.length, data: runs };
+	});
+	
+	setOmnibarHtml(staticText[0])
+	runTickerText();
 
-		else if (type === "prize") {
-			let prizeIndex = Math.floor(Math.random() * value.prizes.length);
-			text.prize = [
-				`<p class="multiLine">Latest Prize: ${value.prizes[prizeIndex].name}</p> <p class="multiLine">Minimum Donation: $${value.prizes[prizeIndex].minDonation}</p>`,
-				`<p class="multiLine">Latest Prize: ${value.prizes[prizeIndex].name}</p> <p class="multiLine">Minimum Donation: $${value.prizes[prizeIndex].minDonation}</p>`,
-			]
-		}
-
-		else if (type === "goal") {
-			text.goal = [
-				`<p class="singleLine">Event Goal: $${value.event.goal}</p>`,
-			]
-		}
-
-		return text[type][index];
-
-	} catch { return false }
-}
-
-function getPlayerNames(run) {
-	let playerArray = [];
-	for (const team of run.teams) {
-		for (const player of team.players) {
-			playerArray.push(player.name);
-		}
+	function setOmnibarHtml(html) {
+		$('#omnibar-content').fadeOut(nodecg.bundleConfig.omnibar.fadeOutTime, () => {
+			$('#omnibar-content').html(html).fadeIn(nodecg.bundleConfig.omnibar.fadeInTime);
+		});
 	}
-	return playerArray.join(', ');
-}
 
+	function runTickerText() {
+		setInterval(() => {
+			if (runIndex < nextRuns.value.dataLength && runIndex < nodecg.bundleConfig.omnibar.numRuns) {
+				runText(runIndex);
+				runIndex++;
+			}
+			else {
+				staticText(staticIndex)
+				staticIndex++;
 
-async function changeOmnibarText() {
-	switch (counter) {
-		case 0: showMessages(); return;
-		case 1: showHost(); return;
-		case 2: showRuns(); return;
-		case 3: showTargets(); return;
-		case 4: showBidwars(); return;
-		case 5: showPrizes(); return;
-		case 6: showGoal(); return;
+				if (staticIndex > staticText.length + 4) { // +4 as quick fix to showing the 6 social platforms in static text, need to figure out why staticText.length only = 1
+					rewardIndex = pollIndex = targetIndex = runIndex = staticIndex = 0;
+				}
+			}
+		}, nodecg.bundleConfig.omnibar.dwellTime);
 	}
-}
-
-async function showMessages() {
-	await new Promise(async (resolve, reject) => {
-		for (let i = 0; i < 100; i++) {
-			await new Promise(async (loopResolve, loopReject) => {
-				let htmlText = await setText('message', i, dataVal)
-				if (htmlText) setOmnibarHtml(htmlText);
-				setTimeout(() => loopResolve(), nodecg.bundleConfig.omnibar.dwellTime)
-			});
-			if (i >= text.message.length - 1) resolve();
-		}
-	})
-	counter++;
-	return changeOmnibarText();
-}
-
-async function showHost() {
-	await new Promise(async (resolve, reject) => {
-		if (!data.value.host || !nodecg.bundleConfig.omnibar.showHost) resolve();
-		setOmnibarHtml(await setText('host', 0, dataVal));
-		setTimeout(() => resolve(), nodecg.bundleConfig.omnibar.dwellTime)
-	});
-
-	counter++;
-	return changeOmnibarText();
-}
-
-async function showRuns() {
-	await new Promise(async (resolve, reject) => {
-		for (let i = 0; i < 100; i++) {
-			await new Promise(async (loopResolve, loopReject) => {
-				let htmlText = await setText('run', i, dataVal)
-				if (htmlText && i <= nodecg.bundleConfig.omnibar.numRuns - 1) setOmnibarHtml(htmlText);
-				else { resolve(); return; }
-				setTimeout(() => loopResolve(), nodecg.bundleConfig.omnibar.dwellTime)
-			});
-		}
-	})
-	counter++;
-	return changeOmnibarText();
-}
-
-async function showTargets() {
-	await new Promise(async (resolve, reject) => {
-		for (let i = 0; i < 100; i++) {
-			await new Promise(async (loopResolve, loopReject) => {
-				let htmlText = await setText('target', i, dataVal)
-				if (htmlText && i <= nodecg.bundleConfig.omnibar.numTargets - 1) setOmnibarHtml(htmlText);
-				else { resolve(); return; }
-				setTimeout(() => loopResolve(), nodecg.bundleConfig.omnibar.dwellTime)
-			});
-		}
-	})
-	counter++;
-	return changeOmnibarText();
-}
-
-async function showBidwars() {
-	await new Promise(async (resolve, reject) => {
-		for (let i = 0; i < 100; i++) {
-			await new Promise(async (loopResolve, loopReject) => {
-				let htmlText = await setText('bidwar', i, dataVal)
-				if (htmlText && i <= nodecg.bundleConfig.omnibar.numBidwars - 1) setOmnibarHtml(htmlText);
-				else { resolve(); return; }
-				setTimeout(() => loopResolve(), nodecg.bundleConfig.omnibar.dwellTime)
-			});
-		}
-	})
-	counter++;
-	return changeOmnibarText();
-}
-
-async function showPrizes() {
-	await new Promise(async (resolve, reject) => {
-		for (let i = 0; i < 100; i++) {
-			await new Promise(async (loopResolve, loopReject) => {
-				let htmlText = await setText('prize', i, dataVal)
-				if (htmlText && i <= nodecg.bundleConfig.omnibar.numPrizes - 1) setOmnibarHtml(htmlText);
-				else { resolve(); return; }
-				setTimeout(() => loopResolve(), nodecg.bundleConfig.omnibar.dwellTime)
-			});
-		}
-	})
-	counter++;
-	return changeOmnibarText();
-}
-
-async function showGoal() {
-	await new Promise(async (resolve, reject) => {
-		if (!nodecg.bundleConfig.omnibar.showGoal) resolve();
-		setOmnibarHtml(await setText('goal', 0, dataVal));
-		setTimeout(() => resolve(), nodecg.bundleConfig.omnibar.dwellTime)
-	});
-
-	counter = 0;
-	return changeOmnibarText();
-}
-
-function setOmnibarHtml(html) {
-	$('#omnibarContent').fadeOut(nodecg.bundleConfig.omnibar.fadeOutTime, () => {
-		$('#omnibarContent').html(html).fadeIn(nodecg.bundleConfig.omnibar.fadeInTime);
-	});
-}
+});
